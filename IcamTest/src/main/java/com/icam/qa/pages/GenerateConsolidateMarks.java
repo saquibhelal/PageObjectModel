0274cb2ef8f36430d0a622289ea00fe4eefd8e77
saquibhelal <saquibhelal78@gmail.com>
2018-09-11 14:40:58 +0530

    Author name added....

--- IcamTest/src/main/java/com/icam/qa/pages/GenerateConsolidateMarks.java
+++ IcamTest/src/main/java/com/icam/qa/pages/GenerateConsolidateMarks.java
@@ -10,6 +10,7 @@ import com.icam.qa.base.TestBase;
 
 public class GenerateConsolidateMarks extends TestBase {
 	
+	//@Author Saquib
 	@FindBy(xpath="//select[@name='academicYearCode']")
 	WebElement academicYear;
 	
