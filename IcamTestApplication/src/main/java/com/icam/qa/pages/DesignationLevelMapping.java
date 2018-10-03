3e825da7e78f5aa406bacc87c9a0bf70ba51d2c7
saquibhelal <saquibhelal78@gmail.com>
2018-10-03 16:05:23 +0530

    Changes in DesignationLevelMapping Page....

--- IcamTestApplication/src/main/java/com/icam/qa/pages/DesignationLevelMapping.java
+++ IcamTestApplication/src/main/java/com/icam/qa/pages/DesignationLevelMapping.java
@@ -14,11 +14,8 @@ public class DesignationLevelMapping extends TestBase {
 	@FindBy(css="#designationCode")
 	WebElement designationName;
 	
-	@FindBy(xpath="html/body/section/div/div[1]/form/section/div/div[2]/div/ul/li[2]/input")
+	@FindBy(xpath="html/body/div[1]/section/div/div[1]/form/section/div/div[2]/div/ul/li/input")
 	WebElement check1;
-	
-	@FindBy(xpath="html/body/section/div/div[1]/form/section/div/div[2]/div/ul/li[3]/input")
-	WebElement check2;
 	
 	@FindBy(css=".btn.btn-primary")
 	WebElement submit;
@@ -31,9 +28,8 @@ public class DesignationLevelMapping extends TestBase {
 		driver.switchTo().frame(driver.findElement(By.tagName("iframe")));
 		Thread.sleep(4000);
         Select desigName=new Select(designationName);
-        desigName.selectByVisibleText("TESTER");
+        desigName.selectByVisibleText("AUTOMATION TESTER");
         check1.click();
-        check2.click();
        // submit.click();
 		return new DesignationLevelMapping();
 	}
